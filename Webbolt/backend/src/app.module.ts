ab5acc4b1d57a28062719401cb1a32de451773f4
user.user <example@exemple.hu>
2024-12-16 09:10:54 +0100

    token login

--- Webbolt/backend/src/app.module.ts
+++ Webbolt/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TermekekModule } from './termekek/termekek.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TermekekModule, UsersModule],
+  imports: [TermekekModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
